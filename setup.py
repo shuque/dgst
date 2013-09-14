71f6381f924d31eb36457f2ad411882a85bcddc7
Shumon Huque <shuque@upenn.edu>
2013-09-13 20:57:29 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='dgst',
+      version='0.1',
+      scripts=['dgst'],
+      description='Compute cryptographic hashes and output hexdigest',
+      author='Shumon Huque',
+      author_email='shuque@upenn.edu',
+      url='http://github.com/shuque/dgst',
+
+      long_description = \
+      """dgst is a python program to output cryptographic hashes.""",
+      )
